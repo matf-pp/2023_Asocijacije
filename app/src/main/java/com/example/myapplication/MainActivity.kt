cfdfea20cede8daad662625c036d911817db6038
Tanja Gavrić <tanja2000gavric@gmail.com>
2023-07-16 12:50:28 +0200

    Main activity

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPlay: Button = findViewById(R.id.btnPlay)
+        btnPlay.setOnClickListener(){
+            val intent = Intent(this, ChooseGroupActivity::class.java)
+            startActivity(intent)
+        }
+        val btnInstruction: Button = findViewById(R.id.btnInstructions)
+        btnInstruction.setOnClickListener(){
+            val intent = Intent(this, InstructionsActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
